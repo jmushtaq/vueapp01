ebdfc8bbd8e054754363261a99dbce0c8c815af3
jawaidm <dpawasi@.dpaw.wa.gov.au>
2018-03-27 10:41:09 +0800

    Completed Lesson 17

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1> {{ title }} </h1>
+    <p> {{ greeting() }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'first Vue file!'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'hello';
+    }
+  }
+}
+</script>
+
+<style>
+</style>
